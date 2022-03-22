ae6075124cc5161b2df312fc14f462d2ca06f4d0
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-03-22 13:27:09 +0300

    hw 5 and 6 дополнительные задачи

--- src/ru/stepik/Main.java
+++ src/ru/stepik/Main.java
@@ -9,10 +9,15 @@ public class Main {
         // task2();
         // task3();
         // task4();
-        task5();
-        task6();
-        task7();
-        task8();
+        // task5();
+        // task6();
+        // task7();
+        // task8();
+        task9();
+        task10();
+        task11();
+        task12();
+        task13();
     }
 
     public static void task1() {
@@ -162,8 +167,8 @@ public class Main {
 
     public static void task6() {
         int[] array = generateRandomArray();
-        int min = 200_001;
-        int max = 99_999;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < array.length; i++) {
             if (array[i] < min) {
                 min = array[i];
@@ -190,8 +195,78 @@ public class Main {
     public static void task8() {
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i] + " ");
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
+    }
+
+    public static void task9() {
+        int[][] array = new int[3][3];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = 1;
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 
+    public static void task10() {
+        int[] array = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(array));
+        int[] array1 = new int[array.length];
+        for (int i = array.length - 1; i >= 0; i--) {
+            array1[i] = array[(array.length - 1) - i];
+        }
+        System.out.print(Arrays.toString(array1));
+    }
+
+    public static void task11() {
+        int[] array = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(array));
+        int n = 0;
+        for (int i = 0; i < array.length / 2; i++) {
+            n = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = n;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void task12() {
+        int[] array = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        Arrays.sort(array);
+        int target = -2;
+        int head = 0;
+        int tail = array.length - 1;
+        while (head < tail) {
+            int sum = array[head] + array[tail];
+            if (sum == target) {
+                System.out.println(array[head] + " " + array[tail]);
+                break;
+            } else if (sum < target) {
+                head++;
+            } else {
+                tail--;
+            }
+        }
+    }
+
+    public static void task13() {
+        int[] array = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        Arrays.sort(array);
+        for (int i = 0; i < array.length / 2; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] + array[j] == -2) {
+                    System.out.println(array[i] + " " + array[j]);
+                }
+            }
+        }
+    }
 }
+
+
+
+
+
